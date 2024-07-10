15447e07455b50fc90692b23282a35021fa624dc
Wale Adedokun <Waleadedokun73@gmail.com>
2024-07-10 21:01:18 +0100

    style home page

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,12 +1,28 @@
-const menuBtn = document.querySelector('.btn-menu');
-const closeMenuBtn = document.querySelector('.btn-close');
-const mobileMenu = document.querySelector('.nav')
-console.log(mobileMenu);
+$(document).ready(function() {
+  // Menu Navigation
+  $('.btn-menu').click(function () {
+    $('.nav').addClass('visible');
+  })
+  $('.btn-close').click(function () {
+    $('.nav').removeClass('visible')
+  })
 
-menuBtn.addEventListener('click', () => {
-  mobileMenu.classList.add('visible');
-})
+  
+  $('.navicon.left').click(function(event) {
+    var currentImage =  $('.image-slider img.active');
+    var prevImage = currentImage.prev();
+    if (prevImage.length) {
+        currentImage.removeClass('active').css('z-index', -10);
+        prevImage.addClass('active').css('z-index',10)
+    }
+  })
 
-closeMenuBtn.addEventListener('click', () => {
-  mobileMenu.classList.remove('visible');
+  $('.navicon.right').click(function(event) {
+    var currentImage =  $('.image-slider img.active');
+    var nextImage = currentImage.next();
+    if (nextImage.length) {
+        currentImage.removeClass('active').css('z-index', -10);
+        nextImage.addClass('active').css('z-index',10)
+    }
+  })
 })
